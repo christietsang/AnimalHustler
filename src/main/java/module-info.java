8107141d16e62dc520188d85888b0f51738d60f4
Christie Tsang <ctsang195@gmail.com>
2022-03-03 09:47:37 -0800

    Hello World!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.bcit.comp2522.termproject.christiebelal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.bcit.comp2522.termproject.christiebelal to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.christiebelal;
+}
