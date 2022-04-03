110f4221e325eb5f7f3d74108c918c5686ddd815
Christie Tsang <ctsang195@gmail.com>
2022-04-02 19:42:11 -0700

    Reformat existing code

--- src/main/java/ca/bcit/comp2522/termproject/christiebelal/AnimalHustlerApp.java
+++ src/main/java/ca/bcit/comp2522/termproject/christiebelal/AnimalHustlerApp.java
@@ -5,33 +5,12 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.component.Component;
-import com.almasb.fxgl.entity.level.Level;
-import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.input.virtual.VirtualButton;
-import com.almasb.fxgl.physics.HitBox;
-import com.almasb.fxgl.physics.PhysicsComponent;
-import com.almasb.fxgl.time.Timer;
-import com.almasb.fxgl.time.TimerAction;
-import javafx.scene.layout.VBox;
 import javafx.scene.input.KeyCode;
-import javafx.util.Duration;
-import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.app.GameSettings;
-import javafx.collections.FXCollections;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-
-import javafx.scene.control.Button;
-
-
-import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static ca.bcit.comp2522.termproject.christiebelal.AnimalHustlerType.*;
-
+import static ca.bcit.comp2522.termproject.christiebelal.Variables.Variables.*;
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 
@@ -128,7 +107,7 @@ public class AnimalHustlerApp extends GameApplication {
     protected void initGame() {
         days = 10;
         getGameWorld().addEntityFactory(new AnimalHustlerFactory());
-        Level level = setLevelFromMap("AnimalHustlerMap.tmx");
+        setLevelFromMap("AnimalHustlerMap.tmx");
         player = spawn("player", 450, 450);
         playerComponent = player.getComponent(PlayerComponent.class);
     }
@@ -142,4 +121,5 @@ public class AnimalHustlerApp extends GameApplication {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
