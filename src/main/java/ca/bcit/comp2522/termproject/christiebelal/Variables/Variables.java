263bf6be777e72b487d699b75be1545076699f79
Christie Tsang <ctsang195@gmail.com>
2022-04-02 19:39:23 -0700

    Create game variables

---
+++ src/main/java/ca/bcit/comp2522/termproject/christiebelal/Variables/Variables.java
@@ -0,0 +1,8 @@
+package ca.bcit.comp2522.termproject.christiebelal.Variables;
+
+public class Variables {
+
+    public static final String MONEY = "money";
+    public static final int MAX_MONEY = 500;
+    public static final String CURRENT_LEVEL = "currentlevel";
+}
