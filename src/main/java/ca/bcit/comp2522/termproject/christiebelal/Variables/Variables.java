f571383cd995a0dbdd0b9f24d265d6d7b258c24c
Christie Tsang <ctsang195@gmail.com>
2022-04-03 20:45:46 -0700

    Increase maximum for max currency earned

--- src/main/java/ca/bcit/comp2522/termproject/christiebelal/Variables/Variables.java
+++ src/main/java/ca/bcit/comp2522/termproject/christiebelal/Variables/Variables.java
@@ -3,7 +3,7 @@ package ca.bcit.comp2522.termproject.christiebelal.Variables;
 public class Variables {
 
     public static final String MONEY = "money";
-    public static final int MAX_MONEY = 500;
+    public static final int MAX_MONEY = 99999;
     public static final String CURRENT_LEVEL = "currentlevel";
     public static int SPAWN_TIMER = 10;
 }
